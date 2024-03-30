20b4ed922905b17b1bbf4d72771bb5dde2d0a31b
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-30 18:04:58 -0500

    Navbar built. Not responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+import { navLinks } from "../../constants";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <nav className="nav">Navbar</nav>;
+  return (
+    <nav className="nav">
+      <img src="/logo.png" alt="logo" />
+      <div className="nav__links">
+        {navLinks.map((link, index) => (
+          <Link key={`navbar-link-${index}`} to={link.route} className="nav__link">
+            {link.label}
+          </Link>
+        ))}
+      </div>
+      <img src="/search.svg" alt="search icon" className="nav__search" />
+    </nav>
+  );
 };
 
 export default Navbar;
